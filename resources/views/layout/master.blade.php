7dd3463dd29e5ea0b08a3f79359d4ba1ee98002e
DutGRIFF <dustin@dutgriff.com>
2015-05-16 16:28:58 -0500

    pull in the right js/css files

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,13 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>DutGRIFF</title>
 
-	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/all.css') }}" rel="stylesheet">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
   <script src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
@@ -27,6 +27,7 @@
 
 	@yield('content')
 
+  <script src="/js/all.css"></script>
   @yield('scripts')
 </body>
 </html>
