4290ca9dfc3b272b3de630101664c5e641e61ce5
DutGRIFF <dustin@dutgriff.com>
2015-05-16 16:22:55 -0500

    Organized views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,97 +1,30 @@
-<html>
-	<head>
-		<title>DutGRIFF</title>
+@extends('layout.master')
 
-		<link href='//fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
+@section('head-content')
+  <link href='css/landing.css' rel='stylesheet' type='text/css'>
+@stop
 
-		<style>
-      body {
-        margin: 0;
-        padding: 0;
-        background-color: black;
-        font-family: 'Lato';
-        font-weight: 300;
-      }
-      .container:before {
-        content: "";
-        position: fixed;
-        left: 0;
-        right: 0;
-        z-index: -1;
-
-        display: block;
-        /*
-          Blurred images can be compressed smaller. I could have used
-          css but it would have been more downloading and processing.
-        */
-        background-image: url({{ asset('images/welcome-background-blur.jpg') }});
-        width: 100%;
-        height: 100%;
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center center;
-
-        opacity: .7;
-      }
-
-      .container {
-        width: 100%;
-        height: 100%;
-      }
-      .content {
-        width: 500px;
-        height: 140px;
-
-        text-align: center;
-        position: fixed;
-        bottom: 30px;
-        right: 30px;
-
-        padding: 20px;
-
-        background-color: rgba(0,0,0,.4);
-      }
-      .text {
-        opacity: 1;
-        color: #19A6FF;
-        background-image: -webkit-linear-gradient(92deg, #19A6FF, #508C7B);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        -webkit-animation: hue 60s infinite linear;
-      }
-      .heading {
-        font-size: 72px;
-        font-weight: 100;
-        margin: 0;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-      }
-
-      @-webkit-keyframes hue {
-        0%, 100% {
-          -webkit-filter: hue-rotate(360deg);
-          filter: hue-rotate(360deg);
-        }
-        50% {
-          -webkit-filter: hue-rotate(180deg);
-          filter: hue-rotate(180deg);
-        }
-      }
-		</style>
-	</head>
-  <!--
-
-    Mobile:
-  -->
-	<body>
-    <div class="container">
-      <div class="content">
-        <div class="text">
-          <h1 class="heading">DutGRIFF.com</h1>
+@section('prepage-content')
+  <div id="landingBody">
+    <div class="landingContainer">
+      <div class="landingContent">
+        <div class="landingText">
+          <h1 class="landingHeading">DutGRIFF.com</h1>
           Let's build something awesome! <br />
           <b><a href="mailto:sites@dutgriff.com">sites@dutgriff.com</a></b>
         </div>
       </div>
     </div>
-	</body>
-</html>
+  </div>
+@stop
+
+@section('content')
+  <div>
+    this is the next section<br />
+    Here
+  </div>
+@stop
+
+@section('scripts')
+  <script src="{{ asset('js/landing.js') }}"></script>
+@stop
