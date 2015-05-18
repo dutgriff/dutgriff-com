373409cc7f39d9f93d9ae0a0d1e7d0e4d43bdebf
DutGRIFF <dustin@dutgriff.com>
2015-05-18 00:22:09 -0500

    More theming and layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -29,6 +29,32 @@
     @yield('content')
   </div>
 
+  <div id="footer">
+    <div class="container">
+      <div class="col-md-3 col-lg-2 col-lg-offset-2">
+        <h2>Socials</h2>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-lg-2">
+        <h2>Work</h2>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <h2>Contact</h2>
+        <ul>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
   <script src="/js/all.js"></script>
   @yield('scripts')
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,32 @@
 @stop
 
 @section('content')
-  this is the next section<br />
-  Here
+  <h1>Welcome to DutGRIFF.com</h1>
+  <p>
+    Hello. My name is Dustin Griffith. I am a freelance software developer with big ambitions and a short attention
+    span. I love to build things.
+  </p>
+  <h1>How about some Lorem Ipsum</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <h1>And again</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <h1>And once more</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
 @stop
 
 @section('scripts')
