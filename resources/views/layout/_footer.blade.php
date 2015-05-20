d0303fb9d994c2c232fb26013087b7832ad4039c
DutGRIFF <dustin@dutgriff.com>
2015-05-19 20:39:06 -0500

    Footer and navbar partials

---
+++ resources/views/layout/_footer.blade.php
@@ -0,0 +1,25 @@
+<div id="footer">
+  <div class="container">
+    <div class="col-md-3 col-lg-2 col-lg-offset-2">
+      <h2>Socials</h2>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <div class="col-md-3 col-lg-2">
+      <h2>Work</h2>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <div class="col-md-6 col-lg-4">
+      <h2>Contact</h2>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</div>
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -21,39 +21,15 @@
 <body>
   @yield('header')
 
-  @yield('prepage-content')
+  @include('layout._navbar')
 
-  @include('layout.navbar')
+  @yield('prepage-content')
 
   <div class="container">
     @yield('content')
   </div>
 
-  <div id="footer">
-    <div class="container">
-      <div class="col-md-3 col-lg-2 col-lg-offset-2">
-        <h2>Socials</h2>
-        <ul>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-      <div class="col-md-3 col-lg-2">
-        <h2>Work</h2>
-        <ul>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-      <div class="col-md-6 col-lg-4">
-        <h2>Contact</h2>
-        <ul>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-  </div>
+  @include('layout._footer')
 
   <script src="/js/all.js"></script>
   @yield('scripts')
