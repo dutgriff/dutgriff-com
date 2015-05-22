b286846dabcd4318d71af78ee960ef5a7346096d
DutGRIFF <dustin@dutgriff.com>
2015-05-22 11:47:32 -0500

    Made inverse more bootstrap like and picked more colors for the theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
     sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </p>
   <div class="row container-inlay-container">
-    <div class="col-xs-12 container-inlay inverse">
+    <div class="col-xs-12 container-inlay inverse inverse-warning">
       <h1>A Big Lorem Ipsum</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
