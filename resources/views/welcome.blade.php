7354cfb88224f86ddc2197d183a8432371bce1d9
DutGRIFF <dustin@dutgriff.com>
2015-07-27 11:38:06 -0500

    Update welcome text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,12 +26,11 @@
 @section('content')
   <h1>Welcome to DutGRIFF.com <span class="h5">(alpha)</span></h1>
   <p>
-    My name is Dustin Griffith. I am a freelance software developer with big ambitions and a
-    short attention span. I love to build things.
+    My name is Dustin Griffith. I am a freelance full stack software developer. I love to build things.
   </p>
   <p>
     This site will someday tell you just about everything you need to know about me and my work. It may be a while
-    before I really get some stuff done here though. I work on it when I can but my clients obviously comes first.
+    before I really get some stuff done here though. I work on it when I can but my clients obviously come first.
   </p>
 
   <div class="row container-inlay-container">
@@ -40,7 +39,7 @@
 
       <p>
         My site will likely be under construction for a while. It may not display right and some things may not work but
-        it am getting there. Once I get something going here I will work out the bugs and start leaving the development
+        I am getting there. Once I get something going here I will work out the bugs and start leaving the development
         branch on a development site so you don't have to see the mess.
       </p>
     </div>
@@ -56,26 +55,21 @@
       Because the theme and layout is constantly changing I focus only on how it looks in Chrome. Once I get a more
       stable theme I will make it work with the most used browsers and mobile devices.
     </dd>
-    <dt>Hire Me Page</dt>
-    <dd>
-      Here we will get more in depth as to what I do and can do for clients. I will show off some of my work as well as
-      give the technical inquirers a rundown of my workflow, languages and capabilities.
-    </dd>
     <dt>Blog</dt>
     <dd>
       Sometimes I just have to tell people. Whether it is something cool I learned, something I see a lot of people
-      doing wrong in development, a workflow that works great for me or just something or just a rant or interest, I
+      doing wrong in development, a workflow that works great for me or just a rant or interest, I
       will tell you all about it here and allow you to give feedback.
     </dd>
     <dt>User Roles</dt>
     <dd>
       Allow users to request and/or be granted specific roles allowing them access to more of the sites features. Right
-      now there aren't any features so this is obviously not a priority. :)
+      now there aren't any features so this is obviously not a priority.
     </dd>
     <dt>Time Punch</dt>
     <dd>
       <b>Demo is up!</b><br/>
-      My <s>AngularJS</s> Vue.js time tracker. This is how I log my hours. It will allows users with a 'client'
+      My <s>AngularJS</s> Vue.js time tracker. This is how I log my hours. It will allow users with a 'client'
       role to see how much time I spend on a project assigned to them and allow them to see in depth where the time
       goes.
     </dd>
@@ -88,6 +82,7 @@
 
   <h2>Looking to hire?</h2>
   <p>
+    <b>Update: I have been really busy lately so please email as soon as possible to lock in your spot on my calendat.</b>
     If you are here in search of a developer feel free to <a href="mailto:sites@dutgriff.com">email me</a>. Whether you
     are looking for a new site, improvements to an existing one, a sophisticated web app or are just curious how I can
     help you or your business, a simple email is all it takes to get us started. Although I am happy to work on small,
@@ -102,8 +97,7 @@
       spend the time and get things done right over leaving broken windows in my code to get it done. That said I
       understand the importance of deadlines and can force myself to spit out hacky code when needed. I have experience in
       many programming/scripting languages and frameworks. 13 out of 16 waking hours every day I am sitting
-      at my computer either studying or programming. I consider myself very resourceful and self sufficient. Despite some
-      previous work with C#, I do not work on Windows outside of testing a site in IE when I have to. <br/>
+      at my computer either studying or programming. I consider myself very resourceful and self sufficient.<br/>
       For more info and specifics ask for my resume or just ask a question.
     </div>
   </div>
